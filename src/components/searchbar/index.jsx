a47ae71ff55aae7c70da45825d4398bc58167b0a
edwardj9 <edwardj525@gmail.com>
2017-11-11 20:39:36 -0800

    Search Filter Error

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -2,7 +2,8 @@ import resources from './page_message.json';
 import globalResources from '../../global/page_message.json';
 
 import React from 'react';
-import { Button, Form, Transition } from 'semantic-ui-react';
+import { Button, Form, Transition , Modal, Popup, Header, Image, Dropdown} from 'semantic-ui-react';
+
 
 export default class Searchbar extends React.Component {
 
@@ -15,8 +16,16 @@ export default class Searchbar extends React.Component {
 		};
 	}
 
+	
+
+	state = { open: false }
+	show = dimmer => () => this.setState({ dimmer, open: true })
+	close = () => this.setState({ open: false })
+
 	render() {
+		const { open, dimmer } = this.state;
 		return (
+
 			<div>
 				<Form onSubmit={this.search.bind(this)}>
 					<Form.Input fluid icon='search' iconPosition='left' onChange={this.isReady.bind(this)} placeholder={resources.search.placeholder} />
@@ -25,6 +34,60 @@ export default class Searchbar extends React.Component {
 						<Button basic={this.state.loading} color={globalResources.color.secondary} content={resources.search.button} fluid type='submit' />
 					</Transition>
 				</Form>
+
+				<Button onClick={this.show('inverted')} size ='small' floated = 'right'>Advanced Search</Button>
+
+				<Modal dimmer={dimmer} open={open} onClose={this.close}>
+		          <Modal.Header>Advanced Search</Modal.Header>
+		          <Modal.Content>
+		            <form class="ui form">
+		            	<label>Recipe Name</label>
+					    <div class="field">
+					      <div class="ui input">
+					        <input type="text" placeholder="Recipe Name" />
+					      </div>
+					    </div>
+
+		            	<label>Exclude Ingredients</label>
+					    <div class="field">
+					      <div class="ui input">
+					        <input type="text" placeholder="Comma separated list ex. 'chicken, milk'" />
+					      </div>
+					    </div>
+
+					    <label>Diet</label>
+					    <div role="combobox" aria-expanded="false" class="ui fluid multiple search selection dropdown">
+						  <input type="text" aria-autocomplete="list" autoComplete="off" class="search" tabIndex="0" value="" />
+						  <span class="sizer"></span>
+						  <div class="default text" role="alert" aria-live="polite">State</div>
+						  <i aria-hidden="true" class="dropdown icon"></i>
+						  <div aria-multiselectable="true" role="listbox" class="menu transition">
+							  <div style="pointer-events:all" role="option" aria-checked="false" aria-selected="true" class="selected item">
+							      <span class="text">Test</span>
+							    </div>
+						  </div>
+						</div>
+
+						<label>Intolerances</label>
+						<div role="combobox" aria-expanded="false" class="ui fluid multiple search selection dropdown">
+						  <input type="text" autoComplete="off" tabIndex="0" value="" />
+						  <span class="sizer"></span>
+						  <div class="default text" role="alert" aria-live="polite">State</div>
+						  <i aria-hidden="true" class="dropdown icon"></i>
+						  <div aria-multiselectable="true" role="listbox" class="menu transition">
+						  </div>
+						</div>
+					</form>
+		          </Modal.Content>
+		          <Modal.Actions>
+		            <Button color='red' onClick={this.close}>
+		              Cancel
+		            </Button>
+		            <Button positive icon='checkmark' labelPosition='right' content="Submit" onClick={this.close} />
+		          </Modal.Actions>
+		        </Modal>
+
+
 			</div>
 		);
 	}
