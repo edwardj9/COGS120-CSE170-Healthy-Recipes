1ded16a03ea7f26be67755300d837cc37df217f5
benjaminltang <benjaminltang@gmail.com>
2017-11-28 00:29:30 -0800

    Test deploy

--- client/src/global/ga.js
+++ client/src/global/ga.js
@@ -1,6 +1,7 @@
 import ReactGA from 'react-ga';
 
 export function init() {
+  console.log(process.env);
   ReactGA.initialize(process.env.GA_KEY);
   ReactGA.pageview(window.location.pathname + window.location.search);
 };
