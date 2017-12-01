f5154bf87338cbac110c0c4608788337f600630f
benjaminltang <benjaminltang@gmail.com>
2017-12-01 11:57:02 -0800

    Add Google Analytics experiment code

--- client/src/global/ga.js
+++ client/src/global/ga.js
@@ -1,14 +1,14 @@
 import ReactGA from 'react-ga';
 
 export function init() {
-	ReactGA.initialize('UA-110505342-1');
-	ReactGA.pageview(window.location.pathname + window.location.search);
+	// ReactGA.initialize('UA-110505342-1');
+	// ReactGA.pageview(window.location.pathname + window.location.search);
 	return Date.now();
 };
 
 export function sendTime(url, start) {
-	ReactGA.event({
-		category: url,
-		action: ((Date.now() - start) / 1000).toString()
-	});
+	// ReactGA.event({
+	// 	category: url,
+	// 	action: ((Date.now() - start) / 1000).toString()
+	// });
 };
